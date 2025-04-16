9e5bb881bfca72277987315c370ee7bb5ff7f075
lubukhu <150521080+lubukhu@users.noreply.github.com>
2025-04-16 13:52:17 +0700

    gộp map

---
+++ Assets/Script/Khoi/MovingPlatform_2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform_2 : MonoBehaviour
+{
+    public float speed = 2f; // Tốc độ di chuyển
+    public float moveDistance = 3f; // Khoảng cách di chuyển lên xuống
+
+    private Vector3 startPosA;
+
+    void Start()
+    {
+        startPosA = transform.position; // Lưu vị trí ban đầu của A
+    }
+
+    void Update()
+    {
+        // Tính toán vị trí mới của A theo trục Y
+        float offset = Mathf.PingPong(Time.time * speed, moveDistance * 2) - moveDistance;
+        transform.position = new Vector3(startPosA.x, startPosA.y + offset, startPosA.z);
+    }
+}
